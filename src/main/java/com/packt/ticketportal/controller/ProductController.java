4e2155f4923612006793e4520f20f2592965d48e
Eczu <mer.kuba@gmail.com>
2016-11-17 13:02:50 +0100

    Refactor productcontroller

--- src/main/java/com/packt/ticketportal/controller/ProductController.java
+++ src/main/java/com/packt/ticketportal/controller/ProductController.java
@@ -1,6 +1,8 @@
 package com.packt.ticketportal.controller;
 
 import com.packt.ticketportal.domain.Ticket;
+import com.packt.ticketportal.domain.repository.impl.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +12,16 @@ import java.math.BigDecimal;
 /**
  * Created by Kuba on 2016-11-17.
  */
+
 @Controller
 public class ProductController {
+    @Autowired
+    private TicketRepository ticketRepository;
+
     @RequestMapping("/tickets")
     public String list(Model model){
-        Ticket systemOfADown = new Ticket("T1234","System of a Down", new BigDecimal(250));
-        systemOfADown.setTicketDesc("Koncert System of A Down");
-        systemOfADown.setTicketCat("Koncert");
-        systemOfADown.setTicketsInStock(3000);
-        model.addAttribute("ticket",systemOfADown);
+
+        model.addAttribute("tickets",ticketRepository.getAllTickets());
         return "tickets";
     }
 }
